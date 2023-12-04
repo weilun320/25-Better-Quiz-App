56e1ca75a7809b3c6274a9cf0ed56d76278eb8fa
Wei Lun <weilun9320@hotmail.com>
2023-12-04 15:51:03 +0000

    Added check answer function and set feedback message function

--- script.js
+++ script.js
@@ -6,3 +6,40 @@ const correctAnswers = [
   "batman",
   "mark-zuckerberg"
 ];
+
+// Check for correct answers
+function checkAnswer() {
+  // Loop through each questions' answer
+  for (let i = 1; i <= 5; i++) {
+    const answers = document.querySelectorAll(`input[name="question-${i}"]`);
+
+    setFeedbackMessage(answers, i);
+  }
+}
+
+// Set feedback message for each question
+function setFeedbackMessage(answers, counter) {
+  // Loop through each answer
+  for (let i = 0; i < answers.length; i++) {
+    const message = document.getElementById(`answer-${counter}`);
+    message.classList.remove("text-success", "text-danger");
+
+    // Check if user selected an answer and the answer is correct
+    if (answers[i].checked && answers[i].value === correctAnswers[counter - 1]) {
+      message.classList.add("text-success");
+      message.innerHTML = "😀 Correct";
+      return;
+    }
+    // Check if user selected an answer but the answer is wrong
+    else if (answers[i].checked && answers[i].value !== correctAnswers[counter - 1]) {
+      message.classList.add("text-danger");
+      message.innerHTML = "😥 Wrong";
+      return;
+    }
+    // User did not select an answer
+    else {
+      message.classList.add("text-danger");
+      message.innerHTML = "Please select an answer";
+    }
+  }
+}
